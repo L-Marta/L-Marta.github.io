bd6e6b89bafffd9ca72cc536f4fac5aa6c661eb4
L-Marta <116031554+L-Marta@users.noreply.github.com>
2022-10-18 20:10:18 +0200

    Update scripthomepage.js

--- js/scripthomepage.js
+++ js/scripthomepage.js
@@ -7,7 +7,7 @@ const dots = Array.from(dotsNav.children);
 const cardWidth = cards[0].getBoundingClientRect().width;
 // Arrange the cards next to one another
 const setCardPosition = (card, index) => {
-  card.style.left = cardWidth * index + 'px';  
+  card.style.left = cardWidth * (index + 3) + 'px';  
 }
 cards.forEach(setCardPosition);
 
