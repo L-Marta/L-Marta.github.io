5321dbed55d9623bf2029a11627d72067089c960
L-Marta <116031554+L-Marta@users.noreply.github.com>
2022-10-18 19:46:06 +0200

    Update scriptHomepage.js

--- js/scriptHomepage.js
+++ js/scriptHomepage.js
@@ -4,6 +4,7 @@ const nextButton = document.querySelector('.carousel__button--right');
 const prevButton = document.querySelector('.carousel__button--left');
 const dotsNav = document.querySelector('.carousel__nav');
 const dots = Array.from(dotsNav.children);
+const cardWidth = cards[0].getBoundingClientRect().width;
 // Arrange the cards next to one another
 const setCardPosition = (card, index) => {
   card.style.left = cardWidth * index + 'px';  
